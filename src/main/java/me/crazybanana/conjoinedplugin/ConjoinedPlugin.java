c66e1f5a002d2ff212807496f319c4a13408bc99
ArvickC <arvick@outlook.com>
2020-11-06 01:12:09 -0800

    First Commit

---
+++ src/main/java/me/crazybanana/conjoinedplugin/ConjoinedPlugin.java
@@ -0,0 +1,17 @@
+package me.crazybanana.conjoinedplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConjoinedPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
